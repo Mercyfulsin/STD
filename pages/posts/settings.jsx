68ccfcea422ffaffa028173f0819ba2697254feb
Mercyfulsin <aruiz858@gmail.com>
2022-01-07 12:43:34 -0800

    Added first skeleton loading bar for awaiting credentials in Settings

--- pages/posts/settings.jsx
+++ pages/posts/settings.jsx
@@ -1,11 +1,19 @@
 import LittleSquare from "../../components/LittleSquare";
 import ContentContainer from "../../components/ContentContainer";
-
-const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
-const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
-const TEST = 'TESt';
+import { useEffect, useState } from "react";
+import { stringify } from "postcss";
+let Creds = {};
 
 const Settings = () => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    console.log("Loading" & loading);
+  }, []);
+
+  GetCredentials(loading, setLoading);
+
   return (
     <ContentContainer
       tnText="Settings"
@@ -13,7 +21,7 @@ const Settings = () => {
       content={
         <>
           <h1 className="text-6xl font-bold mt-20">
-            <a className="text-green-600">STD</a> Settings {TWILIO_ACCOUNT_SID}
+            <a className="text-green-600">STD</a> Settings
           </h1>
           <br />
           <p className="mt-3 text-2xl">
@@ -26,12 +34,9 @@ const Settings = () => {
             <LittleSquare
               title="Twilio Account SID"
               content={
-                <input
-                  className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-                  id="inline-full-name"
-                  type="text"
-                  value={TWILIO_ACCOUNT_SID || "Jane Doe"}
-                  disabled={true}
+                <DataDisplay
+                  val={Creds.TWILIO_ACCOUNT_SID}
+                  load={loading}
                 />
               }
             />
@@ -40,6 +45,30 @@ const Settings = () => {
       }
     />
   );
+};
+
+const DataDisplay = ({ val, load }) => {
+  return load ? (
+    <div class="border border-black shadow rounded-md p-4 max-w-sm w-full mx-auto">
+      <div class="animate-pulse flex space-x-4">
+        <div class="h-10 w-full bg-gray-700 rounded" />
+      </div>
+    </div>
+  ) : (
+    <input
+      className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+      id="inline-full-name"
+      type="text"
+      value={val}
+      disabled={true}
+    />
+  );
+};
+
+const GetCredentials = async (loading, setLoading) => {
+  Creds = await fetch("/api/hello");
+  Creds = await Creds.json();
+  setLoading(false);
 };
 
 export default Settings;
