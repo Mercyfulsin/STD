82fda7b7724e572221cd92ef5e0bc29981c42c51
Mercyfulsin <aruiz858@gmail.com>
2022-01-09 22:58:31 -0800

    Save incoming list to var. check for param 'type' and if ID return list of SIDS only else return the list entirely

--- pages/api/SMS/[smsSID].js
+++ pages/api/SMS/[smsSID].js
@@ -1,15 +1,30 @@
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require('twilio')(accountSid, authToken);
+const client = require("twilio")(accountSid, authToken);
 
-export default function handler(req,res) {
-    // ===== /api/SMS/[smsSID]?qty=#
-    const { smsSID, qty } = req.query;
+async function handler(req, res) {
+  // ===== /api/SMS/[smsSID]?qty=#
+  const { smsSID, qty, type } = req.query;
 
-    if (req.method === 'GET'){
-        res.status(200).json({ SID: smsSID, QTY: qty });
+  if (req.method === "GET") {
+    console.log(type);
+    let temp = await client.messages
+      .list({ limit: parseInt(qty) });
+
+    // ===== /api/SMS/[smsSID]?qty=#&type=id
+    if (type === 'id'){
+      console.log("Following Type ID");
+      let SID = [];
+      temp.forEach((item) => SID.push(item.sid));
+      console.log(SID);
+      res.status(200).json(SID);
     }
-    else if (req.method === 'POST'){
-        console.log(`Post smsSID: ${smsSID}`);
-    }
+
+
+    res.status(200).json(temp);
+  } else if (req.method === "POST") {
+    console.log(`Post smsSID: ${smsSID}`);
+  }
 }
+
+export default handler
