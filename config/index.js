d131c969d30a23e4e2714ec77e495d65c7fd36d0
Mercyfulsin <aruiz858@gmail.com>
2022-01-14 11:50:23 -0800

    changed local port

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://twilio-std.herokuapp.com';
+export const server = dev ? 'http://localhost:3211' : 'https://twilio-std.herokuapp.com';
