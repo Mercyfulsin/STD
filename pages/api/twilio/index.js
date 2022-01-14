0dc50f2157a3bc52fc08b84ebdcbd029f5a6b1f7
Mercyfulsin <aruiz858@gmail.com>
2022-01-14 11:02:04 -0800

    Testing return of status to Twilio

--- pages/api/twilio/index.js
+++ pages/api/twilio/index.js
@@ -14,8 +14,9 @@ export default function handler(req, res) {
     case "POST":
         
         console.log(req.body);
-        
-        res.status(200).json({ 'SMS_SID': req.body.SmsSid });
+        res.writeHead(200, {'Content-Type': 'text/json'});
+        res.end({'Received':'True'});
+        console.log("Ended POST");
       break;
     default:
         console.log(`Default: ${body}`);
