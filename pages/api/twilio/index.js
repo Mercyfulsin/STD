1a2b597ae4336914096e71a7968537d5608845fd
Mercyfulsin <aruiz858@gmail.com>
2022-01-13 22:22:44 -0800

    returning SMS SID on callback

--- pages/api/twilio/index.js
+++ pages/api/twilio/index.js
@@ -15,7 +15,7 @@ export default function handler(req, res) {
         
         console.log(req.body);
         
-        res.status(200).json({'CheckLogs': 'TRUE'});
+        res.status(200).json({ 'SMS_SID': req.body.SmsSid });
       break;
     default:
         console.log(`Default: ${body}`);
